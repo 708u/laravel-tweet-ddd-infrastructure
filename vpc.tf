91ecc7a8834797fb50549a5d22079018d20adaa8
708-U <708.u.biz@gmail.com>
2020-05-06 15:04:36 +0000

    Add route table and igw and subnet assciation

--- vpc.tf
+++ vpc.tf
@@ -24,3 +24,27 @@ resource "aws_subnet" "private_subnet_1a" {
     Name = "${var.project}-private-subnet-1a"
   }
 }
+
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+      Name = "${var.project}-igw"
+  }
+}
+
+resource "aws_route_table" "public_route" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_igw.id
+  }
+
+  tags = {
+    Name = "${var.project}-public-route"
+  }
+}
+
+resource "aws_route_table_association" "routing_subnet_association" {
+  route_table_id = aws_route_table.public_route.id
+  subnet_id      = aws_subnet.public_subnet_1a.id
+}
