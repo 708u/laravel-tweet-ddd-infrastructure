9fadc7c886598ce4f0ad5646aa047b235c862956
708-U <708.u.biz@gmail.com>
2020-05-06 09:22:17 +0000

    Split resources into indivisual file

---
+++ main.tf
@@ -0,0 +1,7 @@
+# resource "aws_instance" "example" {
+#     ami = "ami-0f310fced6141e627"
+#     instance_type = "t2.micro"
+#     tags = {
+#         Name = "${var.project}-web"
+#     }
+# }
--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,3 @@
-variable "access_key" {}
-variable "secret_key" {}
-
-provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_key
-  region     = var.region
-}
-
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   tags = {
@@ -33,11 +24,3 @@ resource "aws_subnet" "private_subnet_1a" {
     Name = "${var.project}-private-subnet-1a"
   }
 }
-
-# resource "aws_instance" "example" {
-#     ami = "ami-0f310fced6141e627"
-#     instance_type = "t2.micro"
-#     tags = {
-#         Name = "${var.project}-web"
-#     }
-# }
