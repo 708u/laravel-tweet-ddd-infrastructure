f458ebe4afde79ba51719ffe30b518ad3ab5bad6
708-U <708.u.biz@gmail.com>
2020-05-09 14:49:21 +0000

    add web security group

---
+++ ecr.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "web" {
+  name   = "${var.project}-web"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.default_route]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.default_route]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.default_route]
+  }
+
+  tags = {
+    Name = "${var.project}-web"
+  }
+}
