f7b73111c7430989d87503d764609ee1745806e0
708-U <708.u.biz@gmail.com>
2020-05-09 15:17:16 +0000

    Add rds settings

---
+++ rds.tf
@@ -0,0 +1,36 @@
+variable "db_password" {}
+
+resource "aws_security_group" "private_db" {
+  name        = "${var.project}-db"
+  description = "${var.project}-db"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web.id]
+  }
+}
+
+resource "aws_db_subnet_group" "private_db" {
+  name       = "laravel-tweet-ddd-db-group"
+  subnet_ids = ["${aws_subnet.private_db_1a.id}", "${aws_subnet.private_db_1c.id}"]
+  tags = {
+    Name = "${var.project}-db-group"
+  }
+}
+
+resource "aws_db_instance" "db" {
+  identifier             = "laravel-tweet-ddd-db"
+  engine                 = "mysql"
+  engine_version         = "5.7"
+  instance_class         = "db.t2.micro"
+  allocated_storage      = 20
+  name                   = replace(var.project, "-", "_")
+  username               = "admin"
+  password               = var.db_password
+  vpc_security_group_ids = ["${aws_security_group.private_db.id}"]
+  db_subnet_group_name   = aws_db_subnet_group.private_db.name
+  skip_final_snapshot    = true
+}
