cea1d83216bfe2730649866330f0cfc2e1630fa4
708-U <708.u.biz@gmail.com>
2020-05-12 01:08:06 +0000

    Add elasticache resource setting

---
+++ elasticache.tf
@@ -0,0 +1,16 @@
+resource "aws_elasticache_cluster" "main" {
+  cluster_id           = "${var.project}-redis"
+  engine               = "redis"
+  node_type            = "cache.t2.micro"
+  num_cache_nodes      = 1
+  engine_version       = "5.0.6"
+  port                 = 6379
+  parameter_group_name = "default.redis5.0"
+  subnet_group_name    = aws_elasticache_subnet_group.main.name
+}
+
+resource "aws_elasticache_subnet_group" "main" {
+  name        = "${var.project}-cache-subnet"
+  description = "${var.project}-cache-subnet"
+  subnet_ids  = [aws_subnet.private_elasticache_1a.id, aws_subnet.private_elasticache_1c.id]
+}
