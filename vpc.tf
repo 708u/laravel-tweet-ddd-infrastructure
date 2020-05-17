f1982cbddda90d28c3104e40bb05d7b2c1db6292
708-U <708.u.biz@gmail.com>
2020-05-17 13:34:56 +0000

    add instance tenancy setting to vpc

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,8 @@
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   tags = {
     Name = "${var.project}-vpc"
   }
