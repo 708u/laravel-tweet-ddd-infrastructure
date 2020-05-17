cceacfba4a80b46f9377d46763b68f77b16bf487
708-U <708.u.biz@gmail.com>
2020-05-17 04:58:44 +0000

    Add ecr image lifecycle policy

--- ecr.tf
+++ ecr.tf
@@ -5,3 +5,13 @@ resource "aws_ecr_repository" "app" {
 resource "aws_ecr_repository" "nginx" {
   name = "${var.project}-nginx"
 }
+
+resource "aws_ecr_lifecycle_policy" "app_policy" {
+  repository = aws_ecr_repository.app.name
+  policy = file("./policies/ecr/lifecycle.json")
+}
+
+resource "aws_ecr_lifecycle_policy" "nginx_policy" {
+  repository = aws_ecr_repository.nginx.name
+  policy = file("./policies/ecr/lifecycle.json")
+}
