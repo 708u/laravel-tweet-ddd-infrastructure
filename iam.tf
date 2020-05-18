fce8f17fc8de72db4634657188b696a4a49ec530
708-U <708.u.biz@gmail.com>
2020-05-18 00:31:06 +0000

    Add iam role of ecs task assume

--- iam.tf
+++ iam.tf
@@ -1,3 +1,4 @@
+# ECR deployer Role
 resource "aws_iam_user" "deploy-user" {
   name = "deploy-user"
 }
@@ -14,16 +15,11 @@ resource "aws_iam_user_policy_attachment" "deploy_attach" {
   policy_arn = aws_iam_policy.deploy.arn
 }
 
+# EC2 instance Role
 resource "aws_iam_role" "ecs_instance_role" {
   name               = "ecs_instance_role"
   path               = "/"
   assume_role_policy = file("policies/iam/ec2_assume_role_policy.json")
-}
-
-resource "aws_iam_role" "ecs_task_role" {
-  name               = "ecs-task-role"
-  path               = "/"
-  assume_role_policy = file("policies/iam/ecs_task_assume_role_policy.json")
 }
 
 resource "aws_iam_instance_profile" "ecs_instance_profile" {
@@ -41,4 +37,11 @@ resource "aws_iam_policy" "ecs_instance_policy" {
 resource "aws_iam_role_policy_attachment" "ecs_instance_role_attach" {
   role       = aws_iam_role.ecs_instance_role.name
   policy_arn = aws_iam_policy.ecs_instance_policy.arn
+}
+
+# ECS task Role
+resource "aws_iam_role" "ecs_task_role" {
+  name               = "ecs-task-role"
+  path               = "/"
+  assume_role_policy = file("policies/iam/ecs_task_assume_role_policy.json")
 }
