c068f74b1ac4276a7f9a896bb7cba303f881947e
708-U <708.u.biz@gmail.com>
2020-05-21 14:45:33 +0000

    add assets bucket in s3

--- s3.tf
+++ s3.tf
@@ -6,3 +6,15 @@ resource "aws_s3_bucket" "main" {
     enabled = false
   }
 }
+resource "aws_s3_bucket" "assets" {
+  bucket = "${var.project}-assets"
+  acl    = "private"
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "HEAD"]
+    allowed_origins = ["https://${var.domain}"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 4000
+  }
+}
