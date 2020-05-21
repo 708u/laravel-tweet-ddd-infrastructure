5fca4bccfe25ffe4e756926a0700392dbceefde3
708-U <708.u.biz@gmail.com>
2020-05-21 12:04:58 +0000

    add tasc definition role and app key secret

--- ecs.tf
+++ ecs.tf
@@ -24,6 +24,7 @@ resource "aws_ecs_service" "app_service" {
 resource "aws_ecs_task_definition" "app" {
   family        = "${var.project}-app"
   task_role_arn = aws_iam_role.ecs_task_role.arn
+  execution_role_arn = aws_iam_role.ecs_task_role.arn
   network_mode  = "awsvpc"
 
   container_definitions = <<EOF
@@ -31,7 +32,7 @@ resource "aws_ecs_task_definition" "app" {
   {
     "name": "app",
     "image": "${aws_ecr_repository.app.repository_url}:latest",
-    "cpu": 400,
+    "cpu": 333,
     "memoryReservation": 400,
     "essential": true,
     "networkMode": "awsvpc",
@@ -59,7 +60,13 @@ resource "aws_ecs_task_definition" "app" {
         "value": "${aws_elasticache_cluster.main.cache_nodes.0.address}"
       }
     ],
-     "logConfiguration": {
+    "secrets": [
+      {
+        "name": "APP_KEY",
+        "valueFrom": "/app/key"
+      }
+    ],
+    "logConfiguration": {
       "logDriver": "awslogs",
       "options": {
         "awslogs-group": "${var.project}-app",
@@ -71,8 +78,8 @@ resource "aws_ecs_task_definition" "app" {
   {
     "name": "nginx",
     "image": "${aws_ecr_repository.nginx.repository_url}:latest",
-    "cpu": 50,
-    "memoryReservation": 100,
+    "cpu": 333,
+    "memoryReservation": 333,
     "essential": true,
     "networkMode": "awsvpc",
     "portMappings": [
