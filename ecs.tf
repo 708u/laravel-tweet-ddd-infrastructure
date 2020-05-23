15e7d9276c85a8ebbcb5660a7c1dd63132ec9f9d
708-U <708.u.biz@gmail.com>
2020-05-23 10:34:51 +0000

    Add migration task

--- ecs.tf
+++ ecs.tf
@@ -155,3 +155,112 @@ resource "aws_ecs_task_definition" "app" {
 ]
 EOF
 }
+
+resource "aws_ecs_task_definition" "migrate" {
+  family             = "${var.project}-migrate"
+  task_role_arn      = aws_iam_role.ecs_task_role.arn
+  execution_role_arn = aws_iam_role.ecs_task_role.arn
+
+  container_definitions = <<EOF
+[
+  {
+    "name": "app",
+    "image": "${aws_ecr_repository.app.repository_url}:latest",
+    "cpu": 300,
+    "memoryReservation": 300,
+    "essential": true,
+    "command": ["php", "artisan", "migrate", "--force"],
+    "portMappings": [
+      {
+        "containerPort": 9000,
+        "hostPort": 9000
+      }
+    ],
+    "environment": [
+      {
+        "name": "APP_ENV",
+        "value": "production"
+      },
+      {
+        "name": "APP_DEBUG",
+        "value": "true"
+      },
+      {
+        "name": "APP_NAME",
+        "value": "${var.project}"
+      },
+      {
+        "name": "APP_URL",
+        "value": "https://laravel-tweet-ddd.work"
+      },
+      {
+        "name": "ASSET_URL",
+        "value": "https://cdn.laravel-tweet-ddd.work"
+      },
+      {
+        "name": "DB_CONNECTION",
+        "value": "mysql"
+      },
+      {
+        "name": "DB_HOST",
+        "value": "${aws_db_instance.db.address}"
+      },
+      {
+        "name": "DB_PORT",
+        "value": "3306"
+      },
+      {
+        "name": "DB_USERNAME",
+        "value": "${replace(var.project, "-", "_")}"
+      },
+      {
+        "name": "DB_DATABASE",
+        "value": "${replace(var.project, "-", "_")}"
+      },
+      {
+        "name": "REDIS_HOST",
+        "value": "${aws_elasticache_cluster.main.cache_nodes.0.address}"
+      },
+      {
+        "name": "REDIS_PASSWORD",
+        "value": "null"
+      },
+      {
+        "name": "REDIS_PORT",
+        "value": "6379"
+      },
+      {
+        "name": "QUEUE_CONNECTION",
+        "value": "redis"
+      },
+      {
+        "name": "CACHE_DRIVER",
+        "value": "redis"
+      },
+      {
+        "name": "TELESCOPE_ENABLED",
+        "value": "false"
+      }
+    ],
+    "secrets": [
+      {
+        "name": "APP_KEY",
+        "valueFrom": "/app/key"
+      },
+      {
+        "name": "DB_PASSWORD",
+        "valueFrom": "/db/password"
+      }
+    ],
+    "logConfiguration": {
+      "logDriver": "awslogs",
+      "options": {
+        "awslogs-group": "${var.project}-app",
+        "awslogs-region": "ap-northeast-1",
+        "awslogs-stream-prefix": "service"
+      }
+    }
+  }
+]
+EOF
+}
