fbf1d734d4613d198aacd946a30bc8e5f21cf83b
708-U <708.u.biz@gmail.com>
2020-05-23 02:46:43 +0000

    Add A-record ailas of cloudfront to route53

--- route53.tf
+++ route53.tf
@@ -14,6 +14,18 @@ resource "aws_route53_record" "a_record" {
   }
 }
 
+resource "aws_route53_record" "cdn_alias" {
+  zone_id = aws_route53_zone.main.id
+  name    = "cdn.${var.domain}"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.assets.domain_name
+    zone_id                = aws_cloudfront_distribution.assets.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
+
 resource "aws_route53_record" "validation" {
   depends_on = [aws_acm_certificate.main]
   zone_id    = aws_route53_zone.main.id
@@ -21,13 +33,17 @@ resource "aws_route53_record" "validation" {
   type       = "CNAME"
   records    = [aws_acm_certificate.main.domain_validation_options.0.resource_record_value]
   ttl        = 60
+
+  allow_overwrite = true
 }
 
 resource "aws_route53_record" "cdn_validation" {
   depends_on = [aws_acm_certificate.cdn]
   zone_id    = aws_route53_zone.main.id
-  name       = aws_acm_certificate.cdn.domain_validation_options.0.resource_record_name
+  name       = aws_acm_certificate.cdn.domain_validation_options.1.resource_record_name
   type       = "CNAME"
-  records    = [aws_acm_certificate.cdn.domain_validation_options.0.resource_record_value]
+  records    = [aws_acm_certificate.cdn.domain_validation_options.1.resource_record_value]
   ttl        = 60
+
+  allow_overwrite = true
 }
