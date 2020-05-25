da9ed689b8f02140794d179adcf1687f7463ef49
708-U <708.u.biz@gmail.com>
2020-05-25 02:53:22 +0000

    Add ssh public key

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,9 @@ resource "aws_instance" "main" {
   iam_instance_profile        = aws_iam_instance_profile.ecs_profile.name
   user_data                   = file("./user_data.sh")
   associate_public_ip_address = true
+  key_name                    = aws_key_pair.instance_ssh.id
   vpc_security_group_ids      = [aws_security_group.web.id]
+
   root_block_device {
     volume_size = "30"
     volume_type = "gp2"
@@ -15,4 +17,9 @@ resource "aws_instance" "main" {
   tags = {
     Name = "${var.project}-app"
   }
+}
+
+resource "aws_key_pair" "instance_ssh" {
+  key_name   = "${var.project}-ssh-key"
+  public_key = file("./ssh_key/public_Key")
 }
