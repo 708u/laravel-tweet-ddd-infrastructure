6247039609d8e5a02896005b5fdb22df1b1cc91a
708-U <708.u.biz@gmail.com>
2020-05-27 06:40:46 +0000

    install awscli

--- user_data.sh
+++ user_data.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 echo ECS_CLUSTER=laravel-tweet-ddd-cluster >> /etc/ecs/ecs.config
+sudo yum update -y
+sudo yum install unzip -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
