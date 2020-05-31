57ac1550a637ce47e387b863c2723802adae1fb9
708-U <708.u.biz@gmail.com>
2020-05-31 12:23:19 +0000

    refactor policy

--- policy.tf
+++ policy.tf
@@ -11,7 +11,7 @@ data "aws_iam_policy_document" "asset_policy" {
 
   statement {
     actions   = ["s3:ListBucket"]
-    resources = ["${aws_s3_bucket.assets.arn}"]
+    resources = [aws_s3_bucket.assets.arn]
 
     principals {
       type        = "AWS"
