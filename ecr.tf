3eb2b407c5d70025cff7d07f9420946506bb836c
708-U <708.u.biz@gmail.com>
2020-05-31 05:25:02 +0000

    Move ecr lifecycle template to templates dir

--- ecr.tf
+++ ecr.tf
@@ -8,10 +8,10 @@ resource "aws_ecr_repository" "nginx" {
 
 resource "aws_ecr_lifecycle_policy" "app_policy" {
   repository = aws_ecr_repository.app.name
-  policy     = file("./policies/ecr/lifecycle.json")
+  policy     = file("./templates/ecr/lifecycle.json")
 }
 
 resource "aws_ecr_lifecycle_policy" "nginx_policy" {
   repository = aws_ecr_repository.nginx.name
-  policy     = file("./policies/ecr/lifecycle.json")
+  policy     = file("./templates/ecr/lifecycle.json")
 }
