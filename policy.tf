16edef50c31c8dd815622ec4396bcf9795275f5c
708-U <708.u.biz@gmail.com>
2020-05-31 04:37:19 +0000

    Move policy of ecs-assume-role

--- policy.tf
+++ policy.tf
@@ -19,3 +19,15 @@ data "aws_iam_policy_document" "asset_policy" {
     }
   }
 }
+
+data "aws_iam_policy_document" "ec2_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
